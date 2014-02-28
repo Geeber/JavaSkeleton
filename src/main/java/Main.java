2c657a776e5b2dcc4031aa9b05724e3a842b0c85
Kevin Litwack <kevin.litwack@gmail.com>
2014-02-27 22:25:10 -0800

    Created barebones Java app with gradle

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+// package .;
+
+
+/**
+ * Main class for the application.
+ *
+ * @author Kevin Litwack (kevin@kevinlitwack.com)
+ */
+public class Main {
+
+    ///// PUBLIC CONSTANTS /////
+
+    ///// PUBLIC STATIC METHODS /////
+
+    /**
+     * Executes the application with the given arguments.
+     *
+     * @param args The arguments.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+    ///// PUBLIC METHODS //////
+
+    ///// PROTECTED METHODS /////
+
+    ///// PRIVATE TYPES /////
+
+    ///// PRIVATE CONSTANTS /////
+
+    ///// PRIVATE FIELDS /////
+
+    ///// PRIVATE METHODS /////
+
+}
